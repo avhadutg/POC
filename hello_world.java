c71653a8146f4f7d4ff806545705860d326f56bf
avhadutgadhave <50046433+avhadutgadhave@users.noreply.github.com>
2019-11-14 14:48:12 +0530

    Update hello_world.java

--- hello_world.java
+++ hello_world.java
@@ -1,6 +1,6 @@
 class hello_world{
 
 	public static void main(String args[]){
-		System.out.println("hello world");
+		System.out.println("hello world welcomehttps://github.com/avhadutgadhave/POC.git");
 	}
 }
