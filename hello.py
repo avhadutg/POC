d4c56b2503b419211353b061d193907c3322efae
avhadutgadhave <50046433+avhadutgadhave@users.noreply.github.com>
2019-11-14 14:26:49 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def main():
-	print("hello world how r u")
-
+	print("hello world how r u welcome")
+https://github.com/avhadutgadhave/POC.git
 if __name__=="__main__":
 	main()
